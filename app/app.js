380be9d76e608c531ee0bf3c6df29f4f26b26eb6
phohead <wilsonyanglol@gmail.com>
2023-10-12 05:38:07 -0400

    Connect MongoDB Atlas Connection URI

--- app/app.js
+++ app/app.js
@@ -2,7 +2,7 @@
 File Name: COMP229_Lab1
 Student Name: Wilson Yang
 Student Numb: 301195179
-Date: October 03, 2023
+Date: October 11, 2023
 */
 
 var createError = require('http-errors');
@@ -10,6 +10,33 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+var mongoose = require("mongoose")
+
+// Setting up MongoDB Database
+var uri = "mongodb+srv://wyang72:72dyBeNncgZ13Rz7@cluster0.lydchyu.mongodb.net/?retryWrites=true&w=majority"
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
+  async function run() {
+    try {
+      // Connect the client to the server	(optional starting in v4.7)
+      await client.connect();
+      // Send a ping to confirm a successful connection
+      await client.db("admin").command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+      // Ensures that the client will close when you finish/error
+      await client.close();
+    }
+  }
+  run().catch(console.dir);
 
 var indexRouter = require('./routes/index.server.routes');
 var usersRouter = require('./routes/users');
@@ -31,18 +58,6 @@ app.use(express.static("./node_modules"));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// Contact Form Data
-app.post('/process-contact', (req, res) => {
-  // Process the form data here. 
-  const firstName = req.body['first-name'];
-  const lastName = req.body['last-name'];
-  const email = req.body['contact-email'];
-  const phoneNum = req.body['contact-phone'];
-  const message = req.body['contact-message'];
-  //After processing, redirect the user back to the home page
-  res.redirect('/');
-});
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
@@ -62,52 +77,3 @@ app.use(function(err, req, res, next) {
 module.exports = app;
 
 
-
-
-/*var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index.server.routes');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'app', 'views'));
-app.set('view engine', 'ejs');
-
-app.get('/', function(req, res) {
-  res.render('index');
-});
-
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
-*/
--- app/routes/index.server.routes.js
+++ app/routes/index.server.routes.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home Page' });
+  res.render('index', { message: 'Welcome to the Marketplace application.' });
 });
 
 module.exports = router;
